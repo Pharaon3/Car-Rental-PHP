ae4d8e2de58dbbde3e02872e2507d03905751308
Pharaon3 <Pharaon313@outlook.com>
2023-05-01 14:57:48 +0300

    pickDate and dropDate

--- index.php
+++ index.php
@@ -15,34 +15,8 @@ if (isset($_POST['try'])) {
     $date = str_replace('-', '/', $newDate1);  
     $newDate = date("m/d/Y", strtotime($date));  
 
-    echo "___";
-    echo $day;
-    echo "___";
-    echo $days;
-    echo "_date__";
-    echo $date;
-    echo "__newDate__";
-    echo $newDate;
-
-    
-    //Assign a date value
-    $dateVal = $orgDate;
-
-    echo "Today is ", date_format($dateVal, "d-m-Y"), "<br/>";
-
-    //Use date_add() function to add 5 days
-    date_add($dateVal, date_interval_create_from_date_string("50 days"));
-
-    //Display the new date
-    echo "The date after 5 days is ", date_format($dateVal, "d-m-Y"), "<br/>";
-
-    //Use date_add() function to add 2 months 10 days
-    date_add($dateVal, date_interval_create_from_date_string("2 months + 10 days"));  
-
-    //Display the new date
-    echo "The date after 2 months 15 days is ", date_format($dateVal, "d-m-Y");  
-
-
+    $dateVal = $day;
+    $dropDate = date('m/d/Y', strtotime($dateVal. ' + ' . $days . ' days'));
 
     $curl = curl_init();
     curl_setopt_array($curl, array(
@@ -61,7 +35,7 @@ if (isset($_POST['try'])) {
             "pickDate":"' . $date .'",
             "pickTime":"12:00 PM",
             "dropInfo":"ANB",
-            "dropDate":"05/05/2023",
+            "dropDate":"' . $dropDate . '",
             "dropTime":"12:00 PM",
             "couponNumber":"",
             "couponInstances":"",
@@ -79,9 +53,9 @@ if (isset($_POST['try'])) {
             'password: AVISCOM',
             'userName: AVISCOM',
             'Referer: https://www.avis.com/en/locations/us/fl/clearwater',
-            'digital-token: eyJjdHkiOiJKV1QiLCJlbmMiOiJBMjU2R0NNIiwiYWxnIjoiZGlyIn0..3oJvIERqsuSXzSb2.Fn4gtBYohTM61LDBnvGx2Bbvu4YEO_ggYcDPhbhNqk0AAVM9ixVXKWJGwKzLRs8ev_AjO1RMChsiIlagvdO00TL8tmm4C8HWbRqV9N4CSKvudCr_M6s403hCH9VMb7MeivtGzIvZ47DqotUaJtjY_E_vbQ3Jd4pL38FLQrSf9cz5pgbQ7YX5u__jvmYuuLsGIl6BMA-KMK8K_vbTSXRHS5YNx8ZXXWloqeYmp8OCj8b8yJdTK2dg9g60uF3nqygb0J3CMz1yo7zAk9I2e9ivvUXP9VJEBd_3pEq2essssEYCB9ch-1-oRoeDmxg.D7Lvit1v7Z2U5uzIx4_PCQ',
+            'digital-token: eyJjdHkiOiJKV1QiLCJlbmMiOiJBMjU2R0NNIiwiYWxnIjoiZGlyIn0..WZb3e6NdMEvLLyWj.Q5O9PmqeZbpNmEOeYsa66s__dBbaiR8P-WR7z6BL9UbkNy6lKtlVSN-PuKTMsXjXDzWmN_aW9_p7V8F17SMRn-TCJ9hZ6moZcKZgCwnfq-p7_9uteOu85cnf-KuUanK2RlV6IMAMbZl222njPjtxMBW53_nhFFEiXaG27Vl1nSPTSYgOFWFPXqvKjYFmmrSfqGuan1Tu2aJmQ65PdW_s1tOVOgXro4VsDhcrtUxp92XWxezDwza3EvG-zTqAaKI8jO6QWtMWGGMqiiqQ2AXnQyH-inGRZJbQ67almTqUoNhXs0OOfRcPg3wUDJb0marMFMxV6Sx-fhRKFnxMSV7zFgP80edOobUdlly3TMGxMHfrSyHwIm21gvV7SubADDB_KWbGrUrheRgi-ljyDWr4ibgROXQ5e5W1VT2dIrsGjSL5Og_UU_gr2WZsvvtpVmmb7rnIBfNDjMTDnJqEPhMZT_L17FxHwIHefYAos72hQXJVepcGKTp6L27cGQGWlO9Te77HKk_3WcahmnNge90zOLDV6N5zskBIzgD3pRh5Oyr5vPsP_mmJZZlz22ZWQKLpaYjDmtg8MVIinb_-dulR1yxysMxXuZrF9M69-2IAaJ1H2DNcb5f6ZWN5a19EyTF3FuUVQ6tTsQqQIgzq_jO6yWG6-FtFAYPgluGUTn62eSNQFRjQvG46M4stufz-97cMxLdXs_VRLw-EW0nX_cqd9kGUmil4Yx2Pwzb3pwXbebj0zNpsidH3VgxTuDW5Jt-ykoeAGEJ6Lrekg-0wk7OZ9mkWR3SbvMVOVx6PUX1jybFoEpho6Ah0fMcX5CBbZzvugXT6kAZ08KItZsn5Ecoqrd4zUYyvY9HZnMt6SXUUfsNNK2CvQ1cJHGcdDtiDDWt4MPF10WylWH3IbrC3vTO_ytlfQnlqpdGHsSTOqtTrbhkw8adbGEgXupbHh-Jq4tv8V84kt5F0dIKsxIW0tQctQUGfVLYAi3rwP-7o24ecqkM24AhOu1gQjvzVO7NbCh6DDEbW8n5g4cFMRt9JS7qeqnpUAdaDOD38O6JYGT4SmwuHjJh-EsI7z0i0lTZXk54_uAmBUyUBMr_jIaSKW5mzo5qP7tekVkVmrmm_spqfcDz65MglRB7fVhuN4d9F6Sx2aHcxrXb89xLbP2IwhFLr6QH9rwSiAFYhSyHXxopdwibQpdyxHfrZ_Swily5dtPtPThF-lnE4mXg4TLLYuGAw2IbSuAi0sja-3uNSHdFXezvyEru-fBw3Woaq_jCfwIPeKzz0jpj6P9gaorTcV5rWaY1qZIHRsWEH5d0yvwXmSw9yDtm7wC3skawkFwlZWW5BuTxw_iyf5wwOIcE4fyvasTLjyQKa3NvnG1qRLU-9NFJseUMWQr1pHkZvPik-8EIF52qmwvCPSHhU99kj7CbOy4DsFMM.YuBOEqdpZtwwuhDT0OARFg',
             'User-Agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/112.0.0.0 Safari/537.36',
-            'cookie: datacenter=cdal; visitorId=cdal-Afc89ec97-8e87-4024-a6fd-c8ddea2bf9e4; APISID=fb6e1d67-f6b9-43de-89e2-76f4637358ad; JSESSIONID=5xH6NpqCLWlQrEKpGeJkD1pYkMMdK05XCf589W7n.d10vprecmapp01; digital-token=0d4fc32d-6246-4ed9-b5fd-bb54de61c41e-01-cdal-op3814; region=euwest1; akaalb_production_config=~op=avis_euwest1_webapi_private_lbid:avis-webapi-euwest1-aws|avis_com:avis-us-digital-euwest1-aws|~rv=5~m=avis-webapi-euwest1-aws:0|avis-us-digital-euwest1-aws:0|~os=7f956ca2417c5e686d715889b6a30f65~id=b467847cfe97e0c736a0f77ed97dd52b; optimizelyEndUserId=oeu1682930375140r0.7989460495449099; _gcl_au=1.1.552469630.1682930387; _pxhd=28d0847ca44b9371ea45f1de74a945bcf9f2c5f67ed98bbee18de548281fb7ab:ba84edf1-e7fb-11ed-a421-7752726e7a53; _gid=GA1.2.1624059665.1682930390; pxcts=c34791f5-e7fb-11ed-a013-50744b4f6675; _pxvid=ba84edf1-e7fb-11ed-a421-7752726e7a53; __qca=P0-2119111066-1682930399440; QuantumMetricUserID=341865afa8fccda3a7e892952d0ab608; __idcontext=eyJjb29raWVJRCI6IjJQQlZMa3ZIZndKWG5RbzZPQjR4YUVjNlBtNSIsImRldmljZUlEIjoiMlBCVkxqdWYzS0hHdUVkUmE2WnIxWW9ubjdDIiwiaXYiOiIiLCJ2IjoiIn0%3D; _clck=1xxc8tw|1|fb8|0; _fbp=fb.1.1682930412995.5095065; SessionPersistence=PROFILEDATA%3A%3DauthorizableId%253Danonymous; _clsk=8tul6m|1682932492869|2|0|p.clarity.ms/collect; bounceClientVisit2252v=N4IgNgDiBcIBYBcEQM4FIDMBBNAmAYnvgO6kB0AhgG4CWKZAxgPYC2RApgHYcBOPTPIgBYADCJAAaEDxghSxSrXrMWkkHQD6AcyYaU7FChpNOMAGYUw+qZq0Q9BoyfOX9AXyA; QuantumMetricSessionID=c76bff84e136e9f2acb61fa6c4ec7290; _gat_UA-6997633-3=1; _ga_8L27T28KZS=GS1.1.1682932477.2.1.1682932534.3.0.0; _uetsid=bc510230e7fb11ed8e3c6dc094ed8b6d; _uetvid=bc5129c0e7fb11edae8585fa5b26a968; _ga=GA1.2.934796074.1682930390; RT="z=1&dm=avis.com&si=9fcc04d4-4149-46b6-96a2-69fd25cc76f0&ss=lh4mjsyt&sl=0&tt=0&bcn=%2F%2F68794912.akstat.io%2F&nu=43hl6rgh&cl=1204"'
+            'cookie: datacenter=cdal; visitorId=cdal-Afc89ec97-8e87-4024-a6fd-c8ddea2bf9e4; APISID=e6455020-4aa5-47a4-a9e7-ff3fcdae6e70; digital-token=55f34370-ab7e-4575-aa36-272a88e5e015-02-cdal-ho4052; JSESSIONID=5xH6NpqCLWlQrEKpGeJkD1pYkMMdK05XCf589W7n.d10vprecmapp11; region=euwest1; akaalb_production_config=~op=avis_euwest1_webapi_private_lbid:avis-webapi-euwest1-aws|avis_com:avis-us-digital-euwest1-aws|~rv=5~m=avis-webapi-euwest1-aws:0|avis-us-digital-euwest1-aws:0|~os=7f956ca2417c5e686d715889b6a30f65~id=b467847cfe97e0c736a0f77ed97dd52b; optimizelyEndUserId=oeu1682930375140r0.7989460495449099; _gcl_au=1.1.552469630.1682930387; _pxhd=28d0847ca44b9371ea45f1de74a945bcf9f2c5f67ed98bbee18de548281fb7ab:ba84edf1-e7fb-11ed-a421-7752726e7a53; _gid=GA1.2.1624059665.1682930390; pxcts=c34791f5-e7fb-11ed-a013-50744b4f6675; _pxvid=ba84edf1-e7fb-11ed-a421-7752726e7a53; __qca=P0-2119111066-1682930399440; QuantumMetricUserID=341865afa8fccda3a7e892952d0ab608; __idcontext=eyJjb29raWVJRCI6IjJQQlZMa3ZIZndKWG5RbzZPQjR4YUVjNlBtNSIsImRldmljZUlEIjoiMlBCVkxqdWYzS0hHdUVkUmE2WnIxWW9ubjdDIiwiaXYiOiIiLCJ2IjoiIn0%3D; _clck=1xxc8tw|1|fb8|0; _fbp=fb.1.1682930412995.5095065; SessionPersistence=PROFILEDATA%3A%3DauthorizableId%253Danonymous; _clsk=jynpzz|1682941945898|1|1|p.clarity.ms/collect; QuantumMetricSessionID=368a7324ec637644fa62efcd25566176; bounceClientVisit2252v=N4IgNgDiBcIBYBcEQM4FIDMBBNAmAYnvgO6kB0AhgG4CWKZAxgPYC2RApgHZEBO7K7HlQoIaTbrgxEq7ODQZh+IADQgeMEKWKVa9ZixUg6AfQDmTYwJQoxnGADMKYAapOmIl-jfEOnAgL5AA; _gat_UA-6997633-3=1; _px2=eyJ1IjoiY2JiZTYzMTAtZTgxNi0xMWVkLWFmODctNzc3YmMyZWIyYTgxIiwidiI6ImJhODRlZGYxLWU3ZmItMTFlZC1hNDIxLTc3NTI3MjZlN2E1MyIsInQiOjE2ODI5NDIzMTE3NDksImgiOiI0Y2E4OTg4ZmFhYWM3NjRiODNiMjNmZDFlNDZlNzllZTA3YWZjMGRkZmRmZWZjMTNhZDNhMjhiZDE4NTQ3ZDlkIn0=; RT="z=1&dm=avis.com&si=9fcc04d4-4149-46b6-96a2-69fd25cc76f0&ss=lh4s6k51&sl=2&tt=8cm&bcn=%2F%2F68794906.akstat.io%2F&nu=43hl6rgh&cl=23pb"; _ga_8L27T28KZS=GS1.1.1682941948.4.1.1682942057.13.0.0; _uetsid=bc510230e7fb11ed8e3c6dc094ed8b6d; _uetvid=bc5129c0e7fb11edae8585fa5b26a968; _ga=GA1.2.934796074.1682930390'
         ),
     ));
 
@@ -135,7 +109,9 @@ if (isset($_POST['try'])) {
         <input type="button" value="Tomorrow" style="cursor: pointer;" onclick="setTomorrow()">
         <input type="button" value="next month" style="cursor: pointer;" onclick="setMonth()">
         <input type="button" value="custom" style="cursor: pointer;" onclick="setFromCustom()">
-        <input type="button" value="Export" style="cursor: pointer;" onclick="document.getElementById('myForm').submit()">
+        <div>
+            <input type="button" value="Export" style="cursor: pointer;" onclick="document.getElementById('myForm').submit()">
+        </div>  
     </form>  
 </body>
 <script>
