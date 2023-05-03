a35da98cdb55059a1eac8054ff5fbe2c1b178a7b
Pharaon3 <Pharaon313@outlook.com>
2023-05-03 14:47:08 +0300

    google sheet to php

--- index.php
+++ index.php
@@ -205,6 +205,94 @@ if (isset($_POST['try'])) {
             
         </form>  
     </div>
+
+    <div>
+    
+  <form class="gform pure-form pure-form-stacked" method="POST" data-email="example@email.net" target="_blank"
+  action="https://script.google.com/macros/s/AKfycbyR4KDri1HYZHQNhRCu78sz5rJQ1PFP225YwlDCQvLLoSQ_duubNPlgC-jBsfbbrqvT/exec">
+    <!-- change the form action to your script url -->
+
+    <div class="form-elements">
+      <fieldset class="pure-group">
+        <label for="PickDate">PickDate: </label>
+        <input id="PickDate" name="PickDate" />
+      </fieldset>
+
+      <fieldset class="pure-group">
+        <label for="DropDate">DropDate: </label>
+        <input id="DropDate" name="DropDate" />
+      </fieldset>
+
+      <fieldset class="pure-group">
+        <label for="LocationCode">LocationCode: </label>
+        <input id="LocationCode" name="LocationCode" />
+      </fieldset>
+
+      <fieldset class="pure-group">
+        <label for="State">State: </label>
+        <input id="State" name="State" />
+      </fieldset>
+
+      <fieldset class="pure-group">
+        <label for="City">City: </label>
+        <input id="City" name="City" />
+      </fieldset>
+      
+      <fieldset class="pure-group">
+        <label for="Address">Address: </label>
+        <input id="Address" name="Address" />
+      </fieldset>
+
+      <fieldset class="pure-group">
+        <label for="Fulladdress">Fulladdress: </label>
+        <input id="Fulladdress" name="Fulladdress" />
+      </fieldset>
+
+      <fieldset class="pure-group">
+        <label for="Class">Class: </label>
+        <input id="Class" name="Class" />
+      </fieldset>
+
+      <fieldset class="pure-group">
+        <label for="Type">Type: </label>
+        <input id="Type" name="Type" />
+      </fieldset>
+
+      <fieldset class="pure-group">
+        <label for="payNowPrice">payNowPrice: </label>
+        <input id="payNowPrice" name="payNowPrice" />
+      </fieldset>
+
+      <fieldset class="pure-group">
+        <label for="payLaterPrice">payLaterPrice: </label>
+        <input id="payLaterPrice" name="payLaterPrice" />
+      </fieldset>
+
+      <fieldset class="pure-group">
+        <label for="Link">Link: </label>
+        <input id="Link" name="Link" />
+      </fieldset>
+
+      <fieldset class="pure-group honeypot-field">
+        <label for="honeypot">To help avoid spam, utilize a Honeypot technique with a hidden text field; must be empty to submit the form! Otherwise, we assume the user is a spam bot.</label>
+        <input id="honeypot" type="text" name="honeypot" value="" />
+      </fieldset>
+
+      <button class="button-success pure-button button-xlarge">
+        <i class="fa fa-paper-plane"></i>&nbsp;Send</button>
+    </div>
+
+    <!-- Customise the Thankyou Message People See when they submit the form: -->
+    <div class="thankyou_message" style="display:none;">
+      <h2><em>Thanks</em> for contacting us!
+        We will get back to you soon!</h2>
+    </div>
+
+  </form>
+
+  <!-- Submit the Form to Google Using "AJAX" -->
+  <script data-cfasync="false" src="form-submission-handler.js"></script>
+  </div>
 </body>
 <style>
     *{
