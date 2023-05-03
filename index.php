ffc53da5a82c5583866d8a093cd7bc170c195405
Pharaon3 <Pharaon313@outlook.com>
2023-05-03 15:31:55 +0300

    form send

--- index.php
+++ index.php
@@ -80,9 +80,9 @@ if (isset($_POST['try'])) {
                             'password: AVISCOM',
                             'userName: AVISCOM',
                             'Referer: https://www.avis.com/en/locations/us/fl/clearwater',
-                            'digital-token: eyJjdHkiOiJKV1QiLCJlbmMiOiJBMjU2R0NNIiwiYWxnIjoiZGlyIn0..Par2vNy1aRg0BLPC.G4VbeSo9CDx50b6Usv2HN567NxyPgZC9VHsI394UpDNRuaT_BjcVnGGGQw1N-18W5nAJZ0akHjIOrrdEhv4RZBwgqZokQPzSlWcUsH4DkmWaaU0nD9MKxnstbBuLvFyQum3l75oiGxg_TFe0FUiZhSBqeJbHJTVYGE-eOFaym8SQRZA9JE5_XrFxREU4NqPLYwgJGaa7o1lDhKHdctailGXBKOInGQbCd2kZhx73In8ihhC9JnRfFS8LJu41SPjM2-WKPmDoviey694e2F2HRj0LzTAL-_21G1tkOKGKNyE8Dk4_nNH3fNar8kA.fjRmbWvq9m9AwC6hbW4AIA',
+                            'digital-token: eyJjdHkiOiJKV1QiLCJlbmMiOiJBMjU2R0NNIiwiYWxnIjoiZGlyIn0..zOBfTum7_3TzjrDh.a0IyI6X4CSDQgHeL5FOdrUv2NlIsxBdFYctnhplYvCePQQqtkOlXvVKcgLn3z3XtuD-Bnz3Y2Hx6IWxQ8yuhMmUs3vBgUFGL9osoVGRgclkJHVdFPY8ZZp8yaIfjLl1ug24c3G3EoU_K4Gy5nENdBTG7lT--07pAhyjQefbQmqJtbl6hzwD36onjNsf-ducILpo-PC1VigFAIwsve_6z4z9sp0cCQcixLqHMtvW6Afzg0H7NpCDcKkBhb_9jVIyiBOQVhsXMoFvv3cgtPhNjrWqITCoMqPICQ99AGst5DJtEhiioxKKmr3uYzdtBXHqxGyJAhSPZKti-_QjvGb41yHENj89FzAtuI2aC_a9Nguj5keSU25iG4vVBr_HP-rVK09ovU_vEKwVKXl02oFBYAHUPZrmPm_jQ1O8FEiLbkV6ZLUfK61IAX_G21SvMxVoex2FLhkD7tPvNSkPgbUdDx786DWbgwJuE0V1-THdg0SIGHbAP8AbnyM6PVvxdQang23B99UqYrHjmJhDRuqyCXoPLbY80Nojj_YJzWdm5nNe6B7i9aVUlux535457q8C6dllEuzSFkjGXIYCkpJ6zBwk0v_iksztjcuaGR87xq0iTJ0HSIr2Kpkqjyce9QdFCEl-xMWPsgEkU2hO13okkOifr4sgbrVgQvlz2NghpRXIHHDUajPBaDa3UCS-nfPkFjFg13VuQFfcHjxQBO1nSxzl3pAVmHAxS8SxbqAM2Cnf-ZGN9ViRjVkNFBc1itdmKLS1hHRljIDx09jAZI8KwPwMpOKiTcGhpVHC5-6fz6y6TZJhHdG63-RZE-exKhzIA1Hup4DVvzzcaKxOeZNzbQ1GSiF8ebV6T0JjXRURJJ_bd7lg49taGXpN039FBvp29HTLZnqL-guHkhT9zN9Gz-HjFJ1Rnk_ClIDiVi0ECXzUMjY9ToC2FG16UyHsojvBSAwDIcMN-leN6y-Rdwp40PtbY-_9EV5WIawcOL023cKlkPUe5uwEGWNPTKqmv1oKorKEzvUrLLrmCLxtMsJ7GIX0CaGfiMs6UiJWq7oxwWR3OHEGByVmDK7MIcLO7i-fa1k8idh5JN04via7Xit4odHe162LV4kxnUuzmzAMRjNuBhypLrVk5se99lEebxvvXK8kmPMYlVZNKpHc9Ie4uJBuDzLrMVcylxZ2iqsWTSW_gW2HOUxRyPaMXM-hYsyzRj0KOfljr5pu32nbf-xi7Bb4SSGhkKhpkQ8Dgs96Uge1THbo1vzK9l2UGojnFl9xeKDk2ntH7rPZVszMs1PNneN5CV7yQUgMiB6IVXR-0GakVZrA52KeJQyWAUvBZuRT4VHRzXUvhAWSqiAw-ro3uoEPGw6xQtLCsNQGszqOVfj77ts6pWkAk1MWCFO_ZeUTwFvPyFqaXfoawnd8pTqcCeGDjiA.aLIdykg1yU_RMdtyAt63Ow',
                             'User-Agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/112.0.0.0 Safari/537.36',
-                            'cookie: datacenter=cwdc; visitorId=cwdc-A11d57d0c-9e80-4903-8697-d77b1b5ae89f; APISID=5e00ba78-8599-4d33-a255-e52df3dcec11; digital-token=d87ebf1e-1697-444e-9527-02fca5720182-01-cwdc-ho3914; region=useast1; optimizelyEndUserId=oeu1682453900484r0.8156270510527024; _gcl_au=1.1.293317964.1682453933; __qca=P0-867025579-1682453948212; pxcts=72134ae0-e3a6-11ed-8ce7-4e4148547870; _pxvid=721335c3-e3a6-11ed-8ce7-4e4148547870; QuantumMetricUserID=4f01e34040f2c48f318bfb8af495a9b3; _fbp=fb.1.1682453977283.1305752564; akaalb_production_config=~op=avis_useast1_webapi_private_lbid:avis-webapi-useast1-aws|avis_com:avis-us-digital-useast1-aws|~rv=76~m=avis-webapi-useast1-aws:0|avis-us-digital-useast1-aws:0|~os=7f956ca2417c5e686d715889b6a30f65~id=8661c2a99422d53af484f8792993eacd; _gid=GA1.2.2071928034.1683022607; _clck=7g3r8s|1|fb9|0; _pxhd=7ce3f1782a4dc1f72337e94634a8050116935e393a1c8fb16a19ec4eb94a5573:721335c3-e3a6-11ed-8ce7-4e4148547870; SessionPersistence=PROFILEDATA%3A%3DauthorizableId%253Danonymous; _gat_UA-6997633-3=1; _uetsid=726a9010e8d211edbf2a99ca79df9e1f; _uetvid=6e772780e3a611eda306a3d6364fbda2; _ga_8L27T28KZS=GS1.1.1683108879.9.0.1683108879.60.0.0; _ga=GA1.2.48087019.1682453947; RT="z=1&dm=avis.com&si=d4f2f80e-675c-4436-8d2e-031048187205&ss=lh7jjra6&sl=0&tt=0&bcn=%2F%2F02179918.akstat.io%2F&nu=43hl6rgh&cl=uni"'
+                            'cookie: datacenter=cwdc; visitorId=cwdc-A11d57d0c-9e80-4903-8697-d77b1b5ae89f; APISID=cdec4fc0-3261-4dcd-b2f2-95a593282f6f; digital-token=c3f04370-5924-4e5d-a64b-3c8fc7be8384-11-cwdc-ho4106; JSESSIONID=T2kh2_o5WwbwvGYuWJ87GpU8zgU-ZOacFmQ1qsk9.w04vprecmapp01; region=useast1; optimizelyEndUserId=oeu1682453900484r0.8156270510527024; _gcl_au=1.1.293317964.1682453933; __qca=P0-867025579-1682453948212; pxcts=72134ae0-e3a6-11ed-8ce7-4e4148547870; _pxvid=721335c3-e3a6-11ed-8ce7-4e4148547870; QuantumMetricUserID=4f01e34040f2c48f318bfb8af495a9b3; _fbp=fb.1.1682453977283.1305752564; akaalb_production_config=~op=avis_useast1_webapi_private_lbid:avis-webapi-useast1-aws|avis_com:avis-us-digital-useast1-aws|~rv=76~m=avis-webapi-useast1-aws:0|avis-us-digital-useast1-aws:0|~os=7f956ca2417c5e686d715889b6a30f65~id=8661c2a99422d53af484f8792993eacd; _gid=GA1.2.2071928034.1683022607; _pxhd=7ce3f1782a4dc1f72337e94634a8050116935e393a1c8fb16a19ec4eb94a5573:721335c3-e3a6-11ed-8ce7-4e4148547870; SessionPersistence=PROFILEDATA%3A%3DauthorizableId%253Danonymous; _clck=7g3r8s|1|fba|0; QuantumMetricSessionID=bf0c74a977256513505a3b7bc1d75a67; bounceClientVisit2252v=N4IgNgDiBcIBYBcEQM4FIDMBBNAmAYnvgO6kB0AhgG4CWKZAxgPYC2RApgHZEBO7K7HlQoIaTbrgxEq7ODQZh+IADQgeMEKWKVa9ZixUg6AfQDmTYwJQoxnGADMKYAapOmIl-jfEOnAgL5AA; _px2=eyJ1IjoiMDVkMWFjMjAtZTlhYi0xMWVkLTg4NTctZGRkN2U0NDI0YmQxIiwidiI6IjcyMTMzNWMzLWUzYTYtMTFlZC04Y2U3LTRlNDE0ODU0Nzg3MCIsInQiOjE2ODMxMTY0MTI4ODQsImgiOiI3MzljY2M2NjM0YmIyOGJkZTU0ZmEwNzhjNDBmNzEzYTcwYjI1ZmI4ZTFhNjY0OTEzYTI5MTI1YTNhZjUyNTYxIn0=; _clsk=1t76pb|1683116117260|2|1|s.clarity.ms/collect; RT="z=1&dm=avis.com&si=d4f2f80e-675c-4436-8d2e-031048187205&ss=lh7nkj08&sl=1&tt=1hx&bcn=%2F%2F0217991d.akstat.io%2F&nu=43hl6rgh&cl=conc"; _gat_UA-6997633-3=1; _ga_8L27T28KZS=GS1.1.1683115626.11.1.1683116201.7.0.0; _uetsid=726a9010e8d211edbf2a99ca79df9e1f; _uetvid=6e772780e3a611eda306a3d6364fbda2; _ga=GA1.2.48087019.1682453947'
                         ),
                     ));
 
@@ -93,6 +93,11 @@ if (isset($_POST['try'])) {
 
                     $carAmount = count($data->vehicleSummaryList);
                     for ($ca=0; $ca < $carAmount; $ca++) {
+                        $state = $data->reservationSummary->pickLoc->address->state;
+                        $city = $data->reservationSummary->pickLoc->address->city;
+                        $Address = $data->reservationSummary->pickLoc->address->address1;
+                        $Class = $data->vehicleSummaryList[$ca]->carGroup;
+                        $Model = $data->vehicleSummaryList[$ca]->makeModel;
                         $fullAddr = $data->reservationSummary->pickLoc->address->address1 
                                     . $data->reservationSummary->pickLoc->address->city 
                                     . $data->reservationSummary->pickLoc->address->state 
@@ -113,42 +118,73 @@ if (isset($_POST['try'])) {
                             $data->vehicleSummaryList[$ca]->payLaterRate->amount . $data->vehicleSummaryList[$ca]->payLaterRate->currency,
                             $data->webHeader->p13nDetailsMap->referrer,
                         ];
-                        $rows = [$newRow]; // you can append several rows at once
-                        // $valueRange = new \Google_Service_Sheets_ValueRange();
-                        // $valueRange->setValues($rows);
-                        // $range = 'Sheet1'; // the service will detect the last row of this sheet
-                        // $options = ['valueInputOption' => 'USER_ENTERED'];
-                        // $service->spreadsheets_values->append($spreadsheetId, $range, $valueRange, $options);
+                        $rows = [$newRow]; 
+                        
+                        $curl1 = curl_init();
 
-                        echo "Pick Date: ";
-                        echo $date . "<br>";
-                        echo "  Drop Date: ";
-                        echo $dropDate . "<br>";
-                        echo "  Location Code: ";
-                        echo $add . "<br>";
-                        echo "  State: ";
-                        echo $data->reservationSummary->pickLoc->address->state . "<br>";
-                        echo "  City: ";
-                        echo $data->reservationSummary->pickLoc->address->city . "<br>";
-                        echo "  Address: ";
-                        echo $data->reservationSummary->pickLoc->address->address1 . "<br>";
-                        echo "  Full address: ";
-                        echo $data->reservationSummary->pickLoc->address->address1 . " ";
-                        echo $data->reservationSummary->pickLoc->address->city . " ";
-                        echo $data->reservationSummary->pickLoc->address->state . " ";
-                        echo $data->reservationSummary->pickLoc->address->zipCode . " ";
-                        echo $data->reservationSummary->pickLoc->address->country . "<br>";
-                        echo "  Class: ";
-                        echo $data->vehicleSummaryList[$ca]->carGroup . "<br>";
-                        echo "  Type: ";
-                        echo $data->vehicleSummaryList[$ca]->makeModel . "<br>";
-                        echo "  payNowPrice: ";
-                        echo $data->vehicleSummaryList[$ca]->payNowRate->amount . $data->vehicleSummaryList[$ca]->payNowRate->currency . "<br>";
-                        echo "  payLaterPrice: ";
-                        echo $data->vehicleSummaryList[$ca]->payLaterRate->amount . $data->vehicleSummaryList[$ca]->payLaterRate->currency . "<br>";
-                        echo "  Link: ";
-                        echo $data->webHeader->p13nDetailsMap->referrer . "<br>";
-                        echo "<br>";
+                        curl_setopt_array($curl1, array(
+                            CURLOPT_URL => 'https://script.google.com/macros/s/AKfycbyR4KDri1HYZHQNhRCu78sz5rJQ1PFP225YwlDCQvLLoSQ_duubNPlgC-jBsfbbrqvT/exec',
+                            CURLOPT_RETURNTRANSFER => true,
+                            CURLOPT_ENCODING => '',
+                            CURLOPT_MAXREDIRS => 10,
+                            CURLOPT_TIMEOUT => 0,
+                            CURLOPT_FOLLOWLOCATION => true,
+                            CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+                            CURLOPT_CUSTOMREQUEST => 'POST',
+                            CURLOPT_POSTFIELDS => array(
+                                'PickDate' => $date,
+                                'DropDate' => $dropDate,
+                                'LocationCode' => $add,
+                                'State' => $data->reservationSummary->pickLoc->address->state,
+                                'City' => $data->reservationSummary->pickLoc->address->city,
+                                'Address' => $data->reservationSummary->pickLoc->address->address1,
+                                'Fulladdress' => $fullAddr,
+                                'Class' => $data->vehicleSummaryList[$ca]->carGroup,
+                                'Type' => $data->vehicleSummaryList[$ca]->makeModel,
+                                'payNowPrice' => $data->vehicleSummaryList[$ca]->payNowRate->amount . $data->vehicleSummaryList[$ca]->payNowRate->currency,
+                                'payLaterPrice' => $data->vehicleSummaryList[$ca]->payLaterRate->amount . $data->vehicleSummaryList[$ca]->payLaterRate->currency,
+                                'Link' => $data->webHeader->p13nDetailsMap->referrer,
+                                'formDataNameOrder' => '["PickDate","DropDate","LocationCode","State","City","Address","Fulladdress","Class","Type","payNowPrice","payLaterPrice","Link"]',
+                                'formGoogleSheetName' => 'responses',
+                                'formGoogleSendEmail' => 'edmonddantes000313@gmail.com'),
+                            ));
+
+                        $response1 = curl_exec($curl1);
+
+                        curl_close($curl1);
+                        // echo $response;
+
+
+
+                        // echo "Pick Date: ";
+                        // echo $date . "<br>";
+                        // echo "  Drop Date: ";
+                        // echo $dropDate . "<br>";
+                        // echo "  Location Code: ";
+                        // echo $add . "<br>";
+                        // echo "  State: ";
+                        // echo $data->reservationSummary->pickLoc->address->state . "<br>";
+                        // echo "  City: ";
+                        // echo $data->reservationSummary->pickLoc->address->city . "<br>";
+                        // echo "  Address: ";
+                        // echo $data->reservationSummary->pickLoc->address->address1 . "<br>";
+                        // echo "  Full address: ";
+                        // echo $data->reservationSummary->pickLoc->address->address1 . " ";
+                        // echo $data->reservationSummary->pickLoc->address->city . " ";
+                        // echo $data->reservationSummary->pickLoc->address->state . " ";
+                        // echo $data->reservationSummary->pickLoc->address->zipCode . " ";
+                        // echo $data->reservationSummary->pickLoc->address->country . "<br>";
+                        // echo "  Class: ";
+                        // echo $data->vehicleSummaryList[$ca]->carGroup . "<br>";
+                        // echo "  Type: ";
+                        // echo $data->vehicleSummaryList[$ca]->makeModel . "<br>";
+                        // echo "  payNowPrice: ";
+                        // echo $data->vehicleSummaryList[$ca]->payNowRate->amount . $data->vehicleSummaryList[$ca]->payNowRate->currency . "<br>";
+                        // echo "  payLaterPrice: ";
+                        // echo $data->vehicleSummaryList[$ca]->payLaterRate->amount . $data->vehicleSummaryList[$ca]->payLaterRate->currency . "<br>";
+                        // echo "  Link: ";
+                        // echo $data->webHeader->p13nDetailsMap->referrer . "<br>";
+                        // echo "<br>";
                     }
                     // echo "<br>";
                 }
@@ -205,94 +241,6 @@ if (isset($_POST['try'])) {
             
         </form>  
     </div>
-
-    <div>
-    
-  <form class="gform pure-form pure-form-stacked" method="POST" data-email="example@email.net" target="_blank"
-  action="https://script.google.com/macros/s/AKfycbyR4KDri1HYZHQNhRCu78sz5rJQ1PFP225YwlDCQvLLoSQ_duubNPlgC-jBsfbbrqvT/exec">
-    <!-- change the form action to your script url -->
-
-    <div class="form-elements">
-      <fieldset class="pure-group">
-        <label for="PickDate">PickDate: </label>
-        <input id="PickDate" name="PickDate" />
-      </fieldset>
-
-      <fieldset class="pure-group">
-        <label for="DropDate">DropDate: </label>
-        <input id="DropDate" name="DropDate" />
-      </fieldset>
-
-      <fieldset class="pure-group">
-        <label for="LocationCode">LocationCode: </label>
-        <input id="LocationCode" name="LocationCode" />
-      </fieldset>
-
-      <fieldset class="pure-group">
-        <label for="State">State: </label>
-        <input id="State" name="State" />
-      </fieldset>
-
-      <fieldset class="pure-group">
-        <label for="City">City: </label>
-        <input id="City" name="City" />
-      </fieldset>
-      
-      <fieldset class="pure-group">
-        <label for="Address">Address: </label>
-        <input id="Address" name="Address" />
-      </fieldset>
-
-      <fieldset class="pure-group">
-        <label for="Fulladdress">Fulladdress: </label>
-        <input id="Fulladdress" name="Fulladdress" />
-      </fieldset>
-
-      <fieldset class="pure-group">
-        <label for="Class">Class: </label>
-        <input id="Class" name="Class" />
-      </fieldset>
-
-      <fieldset class="pure-group">
-        <label for="Type">Type: </label>
-        <input id="Type" name="Type" />
-      </fieldset>
-
-      <fieldset class="pure-group">
-        <label for="payNowPrice">payNowPrice: </label>
-        <input id="payNowPrice" name="payNowPrice" />
-      </fieldset>
-
-      <fieldset class="pure-group">
-        <label for="payLaterPrice">payLaterPrice: </label>
-        <input id="payLaterPrice" name="payLaterPrice" />
-      </fieldset>
-
-      <fieldset class="pure-group">
-        <label for="Link">Link: </label>
-        <input id="Link" name="Link" />
-      </fieldset>
-
-      <fieldset class="pure-group honeypot-field">
-        <label for="honeypot">To help avoid spam, utilize a Honeypot technique with a hidden text field; must be empty to submit the form! Otherwise, we assume the user is a spam bot.</label>
-        <input id="honeypot" type="text" name="honeypot" value="" />
-      </fieldset>
-
-      <button class="button-success pure-button button-xlarge">
-        <i class="fa fa-paper-plane"></i>&nbsp;Send</button>
-    </div>
-
-    <!-- Customise the Thankyou Message People See when they submit the form: -->
-    <div class="thankyou_message" style="display:none;">
-      <h2><em>Thanks</em> for contacting us!
-        We will get back to you soon!</h2>
-    </div>
-
-  </form>
-
-  <!-- Submit the Form to Google Using "AJAX" -->
-  <script data-cfasync="false" src="form-submission-handler.js"></script>
-  </div>
 </body>
 <style>
     *{
