82e893176b51b336ab1796965f80f51e1ce4bd3b
Pharaon3 <Pharaon313@outlook.com>
2023-05-10 07:18:15 +0300

    api

---
+++ api.php
@@ -0,0 +1,127 @@
+<?php
+// $mycookie = 'datacenter=cwdc; visitorId=cwdc-A30671a44-471f-41bd-8cad-3daef295e31b; APISID=30d9ceca-eadb-4d16-b481-2b97d5cb5aa2; digital-token=98470088-a850-4964-92b1-10ad70e94557-02-cwdc-ho3859; region=useast1; optimizelyEndUserId=oeu1683558557050r0.0976546555374116; akaalb_production_config=~op=avis_useast1_webapi_private_lbid:avis-webapi-useast1-aws|avis_com:avis-us-digital-useast1-aws|~rv=67~m=avis-webapi-useast1-aws:0|avis-us-digital-useast1-aws:0|~os=7f956ca2417c5e686d715889b6a30f65~id=0ad224a7ce787f6ba26a479a0460d2e3; _pxhd=9f90cd42a3c513cd008b13c831a1bc025ebb7d1e43d57404e8de308162168353:b5b76ad6-edb2-11ed-9e5c-706a78416f4a; _gcl_au=1.1.1208556564.1683558815; _gid=GA1.2.2019665117.1683558820; _pxvid=b5b76ad6-edb2-11ed-9e5c-706a78416f4a; pxcts=eaf3899e-edb2-11ed-9342-62436d415a4b; QuantumMetricUserID=0465c6e9c976587d441370d6d5dbb31f; __qca=P0-1150698787-1683558833603; _fbp=fb.1.1683558850982.1778528935; SessionPersistence=PROFILEDATA%3A%3DauthorizableId%253Danonymous; _uetsid=e8a06340edb211edb381436783c91f7a; _uetvid=e8a0af00edb211eda286b555c1a32b02; _ga=GA1.2.802961379.1683558820; _clck=1tc5h6n|1|fbg|0; bounceClientVisit2252v=N4IgNgDiBcIBYBcEQM4FIDMBBNAmAYnvgO6kB0AhgG4CWKZAxgPYC2RApgHZEBO7K7HlQoIaTbrgxEq7ODQZh+IADQgeMEKWKVa9ZixUg6AfQDmTYwJQoxnGADMKYAapOmIl-jfEOnAgL5AA; QuantumMetricSessionID=04067eba6b33e6bced529eb1c2dbd10a; _px2=eyJ1IjoiNDY4ODM3ODAtZWU1MC0xMWVkLTgwNTAtOTU2ZjM3YWMwNmFiIiwidiI6ImI1Yjc2YWQ2LWVkYjItMTFlZC05ZTVjLTcwNmE3ODQxNmY0YSIsInQiOjE2ODM2Mjc2NzI0MTYsImgiOiJhYjk1NmMyMTQ1NTUxNzYwZjk2MzU2NzI4YjUxMDJiY2FiZDczMzc4NDkxZWJmYmNhZDNmZWE0N2VjNjFiOTI0In0=; _gat_UA-6997633-3=1; _clsk=1c0qvum|1683627495638|2|1|s.clarity.ms/collect; _ga_8L27T28KZS=GS1.1.1683626405.3.0.1683627497.60.0.0; RT="z=1&dm=avis.com&si=71f1d09b-b765-4c45-b99b-499a3152df1f&ss=lhg3oini&sl=0&tt=0&bcn=%2F%2F02179919.akstat.io%2F&nu=43hl6rgh&cl=nu1l"';
+$mycookie0 = 'datacenter=';
+$mycookie1 = '; visitorId=cwdc-A30671a44-471f-41bd-8cad-3daef295e31b; APISID=30d9ceca-eadb-4d16-b481-2b97d5cb5aa2; digital-token=';
+$mycookie2 = '; region=useast1; optimizelyEndUserId=oeu1683558557050r0.0976546555374116; akaalb_production_config=~op=avis_useast1_webapi_private_lbid:avis-webapi-useast1-aws|avis_com:avis-us-digital-useast1-aws|~rv=67~m=avis-webapi-useast1-aws:0|avis-us-digital-useast1-aws:0|~os=7f956ca2417c5e686d715889b6a30f65~id=0ad224a7ce787f6ba26a479a0460d2e3; _pxhd=';
+$mycookie3 = '; _gcl_au=1.1.1208556564.1683558815; _gid=GA1.2.2019665117.1683558820; _pxvid=b5b76ad6-edb2-11ed-9e5c-706a78416f4a; pxcts=eaf3899e-edb2-11ed-9342-62436d415a4b; QuantumMetricUserID=0465c6e9c976587d441370d6d5dbb31f; __qca=P0-1150698787-1683558833603; _fbp=fb.1.1683558850982.1778528935; SessionPersistence=PROFILEDATA%3A%3DauthorizableId%253Danonymous; _uetsid=e8a06340edb211edb381436783c91f7a; _uetvid=e8a0af00edb211eda286b555c1a32b02; _ga=GA1.2.802961379.1683558820; _clck=1tc5h6n|1|fbg|0; bounceClientVisit2252v=N4IgNgDiBcIBYBcEQM4FIDMBBNAmAYnvgO6kB0AhgG4CWKZAxgPYC2RApgHZEBO7K7HlQoIaTbrgxEq7ODQZh+IADQgeMEKWKVa9ZixUg6AfQDmTYwJQoxnGADMKYAapOmIl-jfEOnAgL5AA; QuantumMetricSessionID=04067eba6b33e6bced529eb1c2dbd10a; _px2=eyJ1IjoiNDY4ODM3ODAtZWU1MC0xMWVkLTgwNTAtOTU2ZjM3YWMwNmFiIiwidiI6ImI1Yjc2YWQ2LWVkYjItMTFlZC05ZTVjLTcwNmE3ODQxNmY0YSIsInQiOjE2ODM2Mjc2NzI0MTYsImgiOiJhYjk1NmMyMTQ1NTUxNzYwZjk2MzU2NzI4YjUxMDJiY2FiZDczMzc4NDkxZWJmYmNhZDNmZWE0N2VjNjFiOTI0In0=; _gat_UA-6997633-3=1; _clsk=1c0qvum|1683627495638|2|1|s.clarity.ms/collect; _ga_8L27T28KZS=GS1.1.1683626405.3.0.1683627497.60.0.0; RT="z=1&dm=avis.com&si=71f1d09b-b765-4c45-b99b-499a3152df1f&ss=lhg3oini&sl=0&tt=0&bcn=%2F%2F02179919.akstat.io%2F&nu=43hl6rgh&cl=nu1l"';
+$mycookie = 'datacenter=cwdc; visitorId=cwdc-A30671a44-471f-41bd-8cad-3daef295e31b; APISID=6a371c26-4acc-49b7-ab9b-22abe8ab2a70; digital-token=bf077d39-508f-40bd-85ab-8eadfbf5825b-01-cwdc-ho5711; region=useast1; optimizelyEndUserId=oeu1683558557050r0.0976546555374116; akaalb_production_config=~op=avis_useast1_webapi_private_lbid:avis-webapi-useast1-aws|avis_com:avis-us-digital-useast1-aws|~rv=67~m=avis-webapi-useast1-aws:0|avis-us-digital-useast1-aws:0|~os=7f956ca2417c5e686d715889b6a30f65~id=0ad224a7ce787f6ba26a479a0460d2e3; _pxhd=9f90cd42a3c513cd008b13c831a1bc025ebb7d1e43d57404e8de308162168353:b5b76ad6-edb2-11ed-9e5c-706a78416f4a; _gcl_au=1.1.1208556564.1683558815; _gid=GA1.2.2019665117.1683558820; _pxvid=b5b76ad6-edb2-11ed-9e5c-706a78416f4a; pxcts=eaf3899e-edb2-11ed-9342-62436d415a4b; QuantumMetricUserID=0465c6e9c976587d441370d6d5dbb31f; __qca=P0-1150698787-1683558833603; _fbp=fb.1.1683558850982.1778528935; SessionPersistence=PROFILEDATA%3A%3DauthorizableId%253Danonymous; _clck=1tc5h6n|1|fbh|0; _ga=GA1.2.802961379.1683558820; _px2=eyJ1IjoiN2EyZTRiNjAtZWVlMS0xMWVkLTg5ODctNmQ2YzFmMDg1OTMzIiwidiI6ImI1Yjc2YWQ2LWVkYjItMTFlZC05ZTVjLTcwNmE3ODQxNmY0YSIsInQiOjE2ODM2OTIxMDA1OTMsImgiOiI1N2UxYTUyZmY5YWY3YTQ2MjVjYmFiOGQ1OWE2NjgzYzhhM2QwZjNmZjIxNzE3NTk5Y2Y0NDU1OGM4NzBhNTdhIn0=; _gat_UA-6997633-3=1; _clsk=ps2yb6|1683691874647|1|1|s.clarity.ms/collect; _ga_8L27T28KZS=GS1.1.1683691876.7.0.1683691876.60.0.0; _uetsid=e8a06340edb211edb381436783c91f7a; _uetvid=e8a0af00edb211eda286b555c1a32b02; RT="z=1&dm=avis.com&si=71f1d09b-b765-4c45-b99b-499a3152df1f&ss=lhh4t1af&sl=0&tt=0&bcn=%2F%2F684dd32e.akstat.io%2F&nu=43hl6rgh&cl=1v8kk"';
+
+$digitoken = 'eyJjdHkiOiJKV1QiLCJlbmMiOiJBMjU2R0NNIiwiYWxnIjoiZGlyIn0..03KVBkm2F3XrRjMX.pgvfsNaMFhrsV1IF12e5vQeCA_IKKz1fCcItKLAf4OJLKRWisTbiIxDha0qlnGuIKSzf00fRqxuT-q-qqGKtqVoa2r0M6Gx8EzmU0t-RkfjAdOeejmcIGaMZzf1XOKdAnT4-Ec7bVB25JEwehbPR2rxATLMqBTU2yAkFUORq1CVBPnDJibUqq652u8FKoDBezSp2mAY4o8ddiaUuBEQ2KPxWUuukOYDGvAF5HUS07kMQDTPAnFJl5uHF-L-dr4Bc-GMpBM1LVluZIp-492cHK8ot37ABuHVKL55r2mv5iX4W5-JAvf_ZnLlXc3Y.FaIOjxusMEsANxSuotGHWg';
+$sheetLink = 'https://script.google.com/macros/s/AKfycbzmbQh3WU2uXMoohIbor8cQ1sKuPp9stwMrKYaC51FqehOrFIYdpDWovkw5Iwu4ypo/exec';
+
+session_start();
+$day = $_GET['day'];
+$days = $_GET['days'];
+
+$orgDate = $day;
+$newDate1 = date("m-d-Y", strtotime($orgDate));
+$date = str_replace('-', '/', $newDate1);
+$newDate = date("m/d/Y", strtotime($date));
+
+$dateVal = $day;
+$dropDate = date('m/d/Y', strtotime($dateVal. ' + ' . $days . ' days'));
+
+$add = $_GET['add'];
+$curl = curl_init();
+curl_setopt_array($curl, array(
+    CURLOPT_URL => 'https://www.avis.com/webapi/reservation/vehicles',
+    CURLOPT_RETURNTRANSFER => true,
+    CURLOPT_ENCODING => '',
+    CURLOPT_MAXREDIRS => 10,
+    CURLOPT_TIMEOUT => 0,
+    CURLOPT_HEADER => 1,
+    // CURLOPT_RETURNTRANSFER => 1,
+    CURLOPT_FOLLOWLOCATION => true,
+    CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+    CURLOPT_CUSTOMREQUEST => 'POST',
+    CURLOPT_POSTFIELDS =>'{
+        "rqHeader":{"brand":"","locale":"en_US"},
+        "nonUSShop":true,
+        "pickInfo":"' . $add . '",
+        "pickDate":"' . $date .'",
+        "pickTime":"12:00 PM",
+        "dropInfo":"' . $add . '",
+        "dropDate":"' . $dropDate . '",
+        "dropTime":"12:00 PM",
+        "couponNumber":"",
+        "couponInstances":"",
+        "couponRateCode":"",
+        "discountNumber":"",
+        "rateType":"",
+        "residency":"US",
+        "age":25,
+        "wizardNumber":"","lastName":"","userSelectedCurrency":"","selDiscountNum":"","promotionalCoupon":"","preferredCarClass":"","membershipId":"","noMembershipAvailable":false,"corporateBookingType":"","enableStrikethrough":"true","amazonGCPayLaterPercentageVal":"","amazonGCPayNowPercentageVal":"","corporateEmailID":""}',
+    CURLOPT_HTTPHEADER => array(
+        'bookingType: car',
+        'channel: Digital',
+        'Content-Type: application/json',
+        'deviceType: bigbrowser',
+        'password: AVISCOM',
+        'userName: AVISCOM',
+        'Referer: https://www.avis.com/en/locations/us/fl/clearwater',
+        'digital-token: ' . $digitoken,
+        'User-Agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/112.0.0.0 Safari/537.36',
+        'cookie: ' . $mycookie
+    ),
+));
+
+$response = curl_exec($curl);
+$datar = json_decode(substr($response, $header_size), false);
+$header_size = curl_getinfo($curl, CURLINFO_HEADER_SIZE);
+$header = substr($response, 0, $header_size);
+curl_close($curl);
+
+
+$carAmount = count($datar->vehicleSummaryList);
+$state = $datar->reservationSummary->pickLoc->address->state;
+$city = $datar->reservationSummary->pickLoc->address->city;
+$Address = $datar->reservationSummary->pickLoc->address->address1;
+$fullAddr = $datar->reservationSummary->pickLoc->address->address1 . " "
+            . $datar->reservationSummary->pickLoc->address->city  . " "
+            . $datar->reservationSummary->pickLoc->address->state  . " "
+            . $datar->reservationSummary->pickLoc->address->zipCode  . " "
+            . $datar->reservationSummary->pickLoc->address->country;
+$Class = $datar->vehicleSummaryList[0]->carGroup;
+$Model = $datar->vehicleSummaryList[0]->makeModel;
+$paynow = $datar->vehicleSummaryList[0]->payNowRate->amount . $datar->vehicleSummaryList[0]->payNowRate->currency;
+$paylater = $datar->vehicleSummaryList[0]->payLaterRate->amount . $datar->vehicleSummaryList[0]->payLaterRate->currency;
+for ($ca=1; $ca < $carAmount; $ca++) {
+    if ($datar->vehicleSummaryList[$ca]->carGroup) $Class = $Class . "," . $datar->vehicleSummaryList[$ca]->carGroup;
+    else $Class = $Class . "," . " ";
+    if ($datar->vehicleSummaryList[$ca]->makeModel) $Model = $Class . "," . $datar->vehicleSummaryList[$ca]->makeModel;
+    else $Model = $Class . ", ";
+    if ($datar->vehicleSummaryList[$ca]->payNowRate->amount) $paynow = $paynow . "," . $datar->vehicleSummaryList[$ca]->payNowRate->amount . $datar->vehicleSummaryList[$ca]->payNowRate->currency;
+    else $paynow = $paynow . ", ";
+    if ($datar->vehicleSummaryList[$ca]->payLaterRate->amount) $paylater = $paylater . "," . $datar->vehicleSummaryList[$ca]->payLaterRate->amount . $datar->vehicleSummaryList[$ca]->payLaterRate->currency;
+    else $paylater = $paylater . ", ";
+}
+$curl1 = curl_init();
+curl_setopt_array($curl1, array(
+    CURLOPT_URL => $sheetLink,
+    CURLOPT_RETURNTRANSFER => true,
+    CURLOPT_ENCODING => '',
+    CURLOPT_MAXREDIRS => 10,
+    CURLOPT_TIMEOUT => 0,
+    CURLOPT_FOLLOWLOCATION => true,
+    CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+    CURLOPT_CUSTOMREQUEST => 'POST',
+    CURLOPT_POSTFIELDS => array(
+        'PickDate' => $date,
+        'DropDate' => $dropDate,
+        'LocationCode' => $add,
+        'State' => $datar->reservationSummary->pickLoc->address->state,
+        'City' => $datar->reservationSummary->pickLoc->address->city,
+        'Fulladdress' => $fullAddr,
+        'Class' => $Class,
+        'Type' => $Model,
+        'payNowPrice' => $paynow,
+        'payLaterPrice' => $paylater,
+        'Link' => $data[$c],
+        'formDataNameOrder' => '["PickDate","DropDate","LocationCode","State","City","Fulladdress","Class","Type","payNowPrice","payLaterPrice","Link"]',
+        'formGoogleSheetName' => 'responses',
+        'formGoogleSendEmail' => 'edmonddantes000313@gmail.com',
+        'amount' => $carAmount),
+    ));
+$response1 = curl_exec($curl1);
+echo $response1;
+?>
